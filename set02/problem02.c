8ac35e6ae8cd4c0f460e1b34c9d614be237120fa
Chandana  B <112924690+Chandanab22@users.noreply.github.com>
2022-12-15 04:09:22 +0000

    solved

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,13 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char str1[20], str2[20];
-    printf("Enter the string: ");
-    scanf("%s", str1);
-    printf("\nString 1 = %s", str1);
-    strcpy(str2, str1);
-    printf("\nString 2 = %s", str2);
-    getch();
-    return 0;
+#include <stdio.h>
+#include <math.h>
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length,float weight);
+
+int main(){
+    float r,l,h,w;
+    input_camel_details(&r,&h,&l);
+    find_weight(r,h,l);
+    output(r,h,l,w);
+}
+
+void input_camel_details(float *radius, float *height, float *length){
+     printf("Enter the radius: \n");
+     scanf("%f",radius);
+     printf("Enter the height: \n");
+     scanf("%f",height);
+     printf("Enter the length: \n");
+     scanf("%f",length);
+     return 0;
+}
+
+
+float find_weight(float radius, float height, float length){
+    float weight = 3.14 * radius * radius * radius * sqrt(height * length);
+    return(weight);
+}
+
+
+void output(float radius, float height, float length, float weight){
+      printf("The weight of the camel with %2.1f,%2.1f,%2.1f is %2.1f\n",radius,height,length,weight);
 }
